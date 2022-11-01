a057410e4b3f8ea5f5e0f2197645337407b64613
Paulo Santos <paulo.jorge.ngs@gmail.com>
2022-11-01 20:00:41 +0000

    Add Routes to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { FC } from 'react';
+import { Routes, Route } from 'react-router';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Homepage from './pages/Homepage';
+import Landing from './pages/Landing';
+import LessonPage from './components/Lesson';
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/home" element={<Homepage />} />
+          <Route path="/lessons" element={<LessonPage />} />
+        </Routes>
+      </Router>
+    </>
   );
-}
+};
 
 export default App;
